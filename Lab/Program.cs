36e10031a8973c76696cf043952a8deaeeedf5e3
Riza <Riza@QQQ>
2016-11-03 17:50:50 +0600

    Add project files.

---
+++ Lab/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    class Program
+    {
+        protected static int q;
+        protected static int n;
+        protected static string city;
+        static void Main(string[] args)
+        {
+            Weather w = new Weather();
+            
+            Console.WriteLine("\n\n\t\tWelcome!!\n\n\t\t");
+            Console.WriteLine("\t\tYou want to see weather for today? Tomorrow? or this week?");
+            Console.WriteLine("\t\tInput 1 for today, 2 for tomorrow, 3 for this week");
+            Console.WriteLine("\t\tBefore inputting number, enter your city");
+            city = Console.ReadLine();
+            int s = Convert.ToInt16(Console.ReadLine());
+            switch (s)
+            {
+                case 1:
+                    w.getWeather(city);
+                    break;
+                case 2:
+                    w.getTomorrow(city);
+                    break;
+                case 3:
+                    n = Convert.ToInt16(Console.ReadLine());
+                    Weather o = new Weather(n,city);
+                    break;                
+            }
+
+        }
+    }
+}
---
+++ Lab/Weather.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    class Weather : Program
+    {
+        protected int z;
+
+        public Weather() { }
+        public Weather(int z, string city) : this(z)
+        {
+            if (this.z != 7)
+            {
+                this.z = 7;
+                getWeather(7, city);
+            }
+        }
+
+        public Weather(int z)
+        {
+            this.z = z;
+        }
+
+        public void getWeather(string city)
+        {
+            Console.WriteLine("Weather for {0}", city);
+            Random rnd = new Random();
+            int q = rnd.Next(-10, 40);
+            if (q <= 0)
+            {
+                Console.WriteLine("Today's temperature is: {0}. {1}", q, "Frosty");
+            }
+            else if (q > 0 &&  q <= 10)
+            {
+                Console.WriteLine("Today's temperature is: {0}. {1}", q, "Cool");
+            }
+            else if (q > 10 && q <= 20)
+            {
+                Console.WriteLine("Today's temperature is: {0}.{1}", q, "warm");
+            }
+            else if (q > 21)
+            {
+                Console.WriteLine("Today's temperature is: {0}.{1}", q, "hot");
+            }
+            Console.ReadKey();
+        }
+
+        public void getTomorrow(string city)
+        {
+            Console.WriteLine("Weather for {0}", city);
+            Random rnd = new Random();
+            int q = rnd.Next(-10, 40);
+            if (q <= 0)
+            {
+                Console.WriteLine("Tomorrow's temperature is: {0}. {1}", q, "Frosty");
+            }
+            else if (q > 0 && q <= 10)
+            {
+                Console.WriteLine("Tomorrow's temperature is: {0}. {1}", q, "Cool");
+            }
+            else if (q > 10 && q <= 20)
+            {
+                Console.WriteLine("Tomorrow's temperature is: {0}.{1}", q, "warm");
+            }
+            else if (q > 21)
+            {
+                Console.WriteLine("Tomorrow's temperature is: {0}.{1}", q, "hot");
+            }
+            Console.ReadKey();
+        }
+
+        public void getWeather(int n, string city)
+        {
+            Console.WriteLine("Weather for {0}", city);
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                int q = rnd.Next(-10, 40);
+                if (q <= 0)
+                {
+                    Console.WriteLine("Day #{0} temperature is: {1}. {2}", i, q, "Frosty");
+                }
+                else if (q > 0 && q <= 10)
+                {
+                    Console.WriteLine("Day #{0} temperature is: {1}. {2}", i, q, "Cool");
+                }
+                else if (q > 10 && q <= 20)
+                {
+                    Console.WriteLine("Day #{0} temperature is: {1}.{2}", i, q, "warm");
+                }
+                else if (q > 21)
+                {
+                    Console.WriteLine("Day #{0} temperature is: {1}.{2}", i,  q, "hot");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+    }
+
